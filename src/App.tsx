40fbbd81e061b0b15a63daf4aa52d493f873fdb3
Stone <b.stone@UK-C02G8437ML85.local>
2023-05-10 14:37:04 +0100

    adds some updates to the basic html

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Howdy partner!</h1>
+      <h1>Howdy Partner!</h1>
       <h3>... and welcome to another application, but this time in React!</h3>
       <p>This version of the site is built with React and TypeScript.</p>
     </div>
